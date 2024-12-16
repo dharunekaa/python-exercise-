6b70bc45c78ae1675e0bc9bd0777bd35df190971
dharunekaa <23bcs016@psgrkcw.ac.in>
2024-12-16 13:36:56 +0530

    Add files via upload

---
+++ program 12 output.py
@@ -0,0 +1,12 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/TEMP.KCW.002/Documents/program 12.py
+enter the limit 5
+enter 1th number :1
+enter 2th number :2
+enter 3th number :3
+enter 4th number :4
+enter 5th number :5
+the sum of entered numbers : 15
+the average of entered numbers : 3.0
