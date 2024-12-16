9b43c863c0f3f97286cc490313beac927b38a48a
dharunekaa <23bcs016@psgrkcw.ac.in>
2024-12-16 13:52:26 +0530

    Add files via upload

---
+++ program 13 output.py
@@ -0,0 +1,29 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/TEMP.KCW.002/Documents/program 13.py
+enter the limit less than 99999999999 23
+enter 1the number:23
+enter 2the number:1
+enter 3the number:2
+enter 4the number:3
+enter 5the number:4
+enter 6the number:5
+enter 7the number:6
+enter 8the number:7
+enter 9the number:8
+enter 10the number:9
+enter 11the number:0
+enter 12the number:11
+enter 13the number:12
+enter 14the number:13
+enter 15the number:14
+enter 16the number:15
+enter 17the number:16
+enter 18the number:17
+enter 19the number:18
+enter 20the number:19
+enter 21the number:20
+enter 22the number:21
+enter 23the number:22
+smallest no.is : 0
