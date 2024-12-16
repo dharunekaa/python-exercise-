13cdf2a98d5d1c3d4b8be12dfff66ea9943ffe3e
dharunekaa <23bcs016@psgrkcw.ac.in>
2024-12-16 14:04:33 +0530

    Add files via upload

---
+++ program 14 output.py
@@ -0,0 +1,12 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/TEMP.KCW.002/Documents/program 14.py
+enter number of elements:6
+enter element:1
+enter element:2
+enter element:3
+enter element:4
+enter element:5
+enter element:6
+second largest element is: 5
