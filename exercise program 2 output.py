5dcbe3dee39a41fd74b4d14e13f5c5218081a455
dharunekaa <23bcs016@psgrkcw.ac.in>
2025-01-27 15:32:11 +0530

    Add files via upload

---
+++ exercise program 2 output.py
@@ -0,0 +1,48 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/2BSCCSA16/Documents/exercise program 1.py
+
+ATM Menu:
+1. Check Balance
+2. Deposit Money
+3. Withdraw Money
+4. Exit
+Enter your choice (1/2/3/4): 1
+Your balance is: $1000
+
+ATM Menu:
+1. Check Balance
+2. Deposit Money
+3. Withdraw Money
+4. Exit
+Enter your choice (1/2/3/4): 2
+Enter deposit amount: $2000
+You deposited: $2000.0. Your new balance is: $3000.0
+
+ATM Menu:
+1. Check Balance
+2. Deposit Money
+3. Withdraw Money
+4. Exit
+Enter your choice (1/2/3/4): 3
+Enter withdrawal amount: $500
+You withdrew: $500.0. Your new balance is: $2500.0
+
+ATM Menu:
+1. Check Balance
+2. Deposit Money
+3. Withdraw Money
+4. Exit
+Enter your choice (1/2/3/4): 4
+Thank you for using the ATM. Goodbye!
+>>> 
+========= RESTART: C:/Users/2BSCCSA16/Documents/exercise program 2.txt =========
+Welcome to the Hotel Food Ordering System!
+
+--- Hotel Menu ---
+1. Pizza - Rs.12
+2. Burger - Rs.8
+3. Pasta - Rs.10
+4. Salad - Rs.6
+5. Exit
